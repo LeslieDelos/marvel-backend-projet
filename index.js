c600782f13d44b550255c8d41cad9d407d758330
Leslie <delos.leslie@gmail.com>
2022-05-15 16:31:28 +0200

    signup-add

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
+//authentification
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+const uid2 = require("uid2");
+
 const app = express();
 
 app.use(cors());
@@ -56,6 +61,65 @@ app.get("/character/:characterId", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.log(error.message);
+  }
+});
+
+//route pour créer un nouveau compte
+app.post("/signup", async (req, res) => {
+  try {
+    const userExist = await User.findOne({ email: req.fields.email });
+
+    if (userExist) {
+      res.status(400).json({ message: "Le mail existe déjà" });
+    } else if (!req.fields.username) {
+      res.status(400).json({ message: "Il manque un pseudo" });
+    } else {
+      const salt = uid2(32);
+      const hash = SHA256(req.fields.password + salt).toString(encBase64);
+      const token = uid2(16);
+      const newUser = new User({
+        email: req.fields.email,
+        account: {
+          username: req.fields.username,
+        },
+        token: token,
+        hash: hash,
+        salt: salt,
+      });
+      await newUser.save();
+      res.status(200).json({
+        _id: newUser._id,
+        token: newUser.token,
+        account: newUser.account,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//deuxieme route pour la connection
+app.post("/login", async (req, res) => {
+  try {
+    const userToCheck = await User.findOne({ email: req.fields.email });
+    if (userToCheck === null) {
+      res.status(401).json({ message: "Unauthorized" });
+    } else {
+      const newHash = SHA256(req.fields.password + userToCheck.salt).toString(
+        encBase64
+      );
+      if (userToCheck.hash === newHash) {
+        res.json({
+          _id: userToCheck._id,
+          token: userToCheck.token,
+          account: userToCheck.account,
+        });
+      } else {
+        res.status(401).json({ message: "Unauthorized !" });
+      }
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
